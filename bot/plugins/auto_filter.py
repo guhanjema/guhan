5aed8adb765e3adb8c5e8127fd5d389782566124
guhanjema <85568893+guhanjema@users.noreply.github.com>
2021-06-15 14:05:47 +0530

    Update auto_filter.py

--- bot/plugins/auto_filter.py
+++ bot/plugins/auto_filter.py
@@ -81,7 +81,7 @@ async def auto_filter(bot, update):
             file_size = "" if file_size == ("[0 B]") else file_size
             
             # add emoji down below inside " " if you want..
-            button_text = f"{file_size}- 📁{file_name}"
+            button_text = f" 📁{file_size}-{file_name}"
             
 
             if file_type == "video":
@@ -147,7 +147,7 @@ async def auto_filter(bot, update):
         if len_result != 1:
             result[0].append(
                 [
-                    InlineKeyboardButton("Next ⏩", callback_data=f"navigate(0|next|{query})")
+                    InlineKeyboardButton("Next 📜", callback_data=f"navigate(0|next|{query})")
                 ]
             )
         
